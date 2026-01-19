e680f4a31d49cab35344ca475e27080b159373e6
mohya972 <mohya972n@gmail.com>
2026-01-19 15:33:49 -0400

    Code pour le chargement de l'API

--- app/components/ecommerce/Products.tsx
+++ app/components/ecommerce/Products.tsx
@@ -1,16 +1,40 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-import { stylesEcommerce } from './stylesEcommerce'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
+import CardProduct from "./CardProduct";
+import { stylesEcommerce } from "./stylesEcommerce";
 
 const Products = () => {
-    // initialisation des variables locales (state)
-    
+  // initialisation des variables locales (state)
+  const [dataProducts, setDataProducts] = useState([]); // tableau de la liste de produits
+  const [loading, setLoading] = useState(true); // état de chargement
 
-    return (
-        <View style={stylesEcommerce.container}>
-            <Text>Products</Text>
-        </View>
-    )
-}
+  // chargement des données de l'API
+  const loadData = async () => {
+    // appel à l'API
+    const query = axios.get("https://fakestoreapi.com/products");
+    console.log("query :", query);
+  };
 
-export default Products
+  useEffect(() => {
+    loadData();
+  }, []); // [] au chargement du composant
+
+  return (
+    <View style={stylesEcommerce.container}>
+      <Text>Nos Produits</Text>
+
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={dataProducts}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <CardProduct />}
+        />
+      )}
+    </View>
+  );
+};
+
+export default Products;
